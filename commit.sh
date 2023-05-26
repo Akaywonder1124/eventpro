22bff0c2bbf3d0898b87d26aa72f82e94c848c80
Akaywonder1124 <akaycopy@gmail.com>
2023-05-26 17:17:29 +0100

    Auto commit at Fri, May 26, 2023  5:17:29 PM

--- commit.sh
+++ commit.sh
@@ -11,7 +11,7 @@ git add -A
 # Commit with a timestamp-based message
 git commit -m "Auto commit at $(date)"
 
-git remote add origin https://github.com/Akaywonder1124/eventpro
+git remote add origin git@github.com:Akaywonder1124/eventpro.git
 
 # Push the changes to the remote repository
 git push -u origin main
